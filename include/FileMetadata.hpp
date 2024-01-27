d0d5296cd3d0b1227a05194425fee5bf98f3880d
Jacky Lee <dr.jackylee@gmail.com>
2024-01-26 23:55:53 -0800

    Add serialize and deserialize methods for FileMetadata

--- include/FileMetadata.hpp
+++ include/FileMetadata.hpp
@@ -1,24 +1,26 @@
 #ifndef FILE_METADATA_HPP
 #define FILE_METADATA_HPP
 
-#include <cstddef>
-#include <vector>
-#include <string>
+#include "ByteBuffer.hpp"
 
-class FileMetadata
-{
+#include <cstddef>
+#include <string>
+#include <vector>
+
+class FileMetadata {
 private:
-    std::uint32_t file_name_length;
-    std::string file_name;
-    std::uint32_t file_size;
-    std::vector<std::byte> file_contents;
+  std::uint32_t file_name_length;
+  std::string file_name;
+  std::uint32_t file_size;
+  std::vector<std::byte> file_contents;
 
 public:
-    FileMetadata(
-        std::uint32_t file_name_length, 
-        const std::string& file_name, 
-        std::uint32_t file_size,
-        std::vector<std::byte>& file_contents);
+  FileMetadata(std::uint32_t file_name_length, const std::string &file_name,
+               std::uint32_t file_size, std::vector<std::byte> &file_contents);
+
+  std::size_t size() const;
+  static FileMetadata Deserialize(const ByteBuffer &byte_buffer);
+  static ByteBuffer Serialize(const FileMetadata &metadata);
 };
 
 #endif
